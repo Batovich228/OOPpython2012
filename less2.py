33da2dcd88ee5bb9e0a428f46a16a312fbb983ba
k308-00 <ebfbb@gmail.com>
2024-03-23 10:56:35 +0200

    hello

--- less2.py
+++ less2.py
@@ -1,6 +1,26 @@
-class Studetnt:
-    print("Hi")
+# class Studetnt:
+#     print("Hi")
+#     def __init__(self):
+#         self.height = 160
+#         print("I am alive!")
+# first = Studetnt()
+
+# class Student:
+#     amount_of_student = 0
+#     def __init__(self, height=160):
+#         self.height = height
+#         Student.amount_of_student += 1
+#
+# nick = Student ()
+# kate = Student (height=170)
+# print(nick.amount_of_student)
+# print(Student.amount_of_student)
+
+class Student:
+    height = 160
     def __init__(self):
-        self.height = 160
-        print("I am alive!")
-first = Studetnt()
+        print(self.height)
+        self.height += 10
+
+nick = Student()
+kate = Student()
