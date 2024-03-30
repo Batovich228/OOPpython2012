34f370ce10f8ce38cfcc3c1a6d15aa5814bed9df
Batovich228 <152063822+Batovich228@users.noreply.github.com>
2024-03-30 13:15:01 +0200

    Update less2.py

--- less2.py
+++ less2.py
@@ -6,10 +6,18 @@ def collector(path, res_path):
     path = os.path.normpath(path)
     for dirpath, dirnames, filenames in os.walk(path):
         for file in filenames:
-            file_time = os.path.getmtime(os.path.join(dirpath, file))
+            file_path = os.path.join(dirpath, file)
+            file_time = os.path.getmtime(file_path)
             datetime_file = datetime.datetime.fromtimestamp(file_time)
             file_date = datetime_file.strftime("%d.%m.%Y")
-            destination_folder = os.path.join(res_path, file_date)
-            if not os.path.exists(destination_folder):
-                os.makedirs(destination_folder)
-            os.replace(os.path.join(dirpath, file), os.path.join(destination_folder, file))
+            target_dir = os.path.join(res_path, file_date)
+            if not os.path.isdir(target_dir):
+                os.makedirs(target_dir)  # Створюємо каталог, якщо його не існує
+            try:
+                os.replace(file_path, os.path.join(target_dir, file))
+            except Exception as e:
+                print(f"Помилка при переміщенні файлу {file}: {e}")
+
+path = "D:/Web"
+res_path = "D:/agent_data"
+collector(path, res_path)
