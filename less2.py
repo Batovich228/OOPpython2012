cb5bf02550c26458b91f1cf4dbeffa0e79f7592c
Batovich228 <152063822+Batovich228@users.noreply.github.com>
2024-03-30 13:11:54 +0200

    Update less2.py

--- less2.py
+++ less2.py
@@ -1,18 +1,15 @@
-import os, datetime
+import os
+import datetime
+
 def collector(path, res_path):
     res_path = os.path.normpath(res_path)
     path = os.path.normpath(path)
     for dirpath, dirnames, filenames in os.walk(path):
         for file in filenames:
-            file_time = os.path.getmtime(f"{dirpath}\\{file}")
+            file_time = os.path.getmtime(os.path.join(dirpath, file))
             datetime_file = datetime.datetime.fromtimestamp(file_time)
             file_date = datetime_file.strftime("%d.%m.%Y")
-            if os.path.isdir(f"{res_path}\\{file_date}"):
-                os.replace(f"{dirpath}\\{file}", f"{res_path}\\{file_date}\\{file}")
-            else:
-                os.mkdir(f"{res_path}\\{file_date}\\")
-            os.replace(f"{dirpath}\\{file}",f"{res_path}\\{file_date}\\{file}")
- 
-path = "D:/Web"
-res_path = "D:/agent_data"
-collector(path, res_path)
+            destination_folder = os.path.join(res_path, file_date)
+            if not os.path.exists(destination_folder):
+                os.makedirs(destination_folder)
+            os.replace(os.path.join(dirpath, file), os.path.join(destination_folder, file))
