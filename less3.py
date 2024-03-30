b51369a23dc34994f1b64afae953b0999576e51c
k308-00 <ebfbb@gmail.com>
2024-03-30 09:39:04 +0200

    hello

--- less3.py
+++ less3.py
@@ -1,24 +1,28 @@
 class Human:
     def __init__(self, name="Human"):
         self.name = name
+
 class Auto:
-    def __init__(self,brand):
+    def __init__(self, brand):
         self.brand = brand
         self.passengers = []
-def add_passenger(self, human):
-    self.passengers.append(human)
 
-def print_passengers_names(self):
-    if self.passengers != []:
-        print(f"Names of {self.brand} passengers: ")
-        for passenger in self.passengers:
-            print(passenger.name)
+    def add_passenger(self, *args):
+        for passenger in args:
+            self.passengers.append(passenger)
+
+    def print_passengers_names(self):
+        if self.passengers:
+            print(f"Names of {self.brand} passengers:")
+            for passenger in self.passengers:
+                print(passenger.name)
         else:
-            print(f"There are no passengers in {self.brand}.")
+            print(f"There are no passengers in {self.brand}")
 
 nick = Human("Nick")
 kate = Human("Kate")
+alex = Human("Alex")
+petrovich = Human("Petrovich")
 car = Auto("Mercedes")
-car.add_passeger(nick)
-car.add_passeger(kate)
-car.print_passeger_names()
+car.add_passenger(nick,kate,alex, petrovich)
+car.print_passengers_names()
