b5b261e414ad7e09f6e582c29b404f8c878411cd
k308-00 <ebfbb@gmail.com>
2024-04-06 11:48:04 +0300

    hellox

--- less6.py
+++ less6.py
@@ -9,19 +9,40 @@
 # #     print("Nuub")
 # print("code after capsule")
 
-try:
-    try:
-        print("Hello")
-        print("error")
-        print("No error")
-    except SyntaxError:
-        print("Wrong Syntax")
-except NameError as error:
-    print(error)
+# try:
+#     try:
+#         print("Hello")
+#         print("error")
+#         print("No error")
+#     except SyntaxError:
+#         print("Wrong Syntax")
+# except NameError as error:
+#     print(error)
+#
+# try:
+#     print('Hello')
+# except:
+#     print("We have problems")
+# else:
+#     print("No problems")
+
+# class BuildingEror(Exception):
+#     def __str__(self):
+#         return f"З такої кількості матеріалу будинок не збудувати!"
+# def check_material(amount_of_material, limit_value):
+#     if amount_of_material > limit_value:
+#         return "enough material"
+#     else:
+#         raise BuildingEror(amount_of_material)
+# materials = 123
+# check_material(materials, 300)
 
 try:
-    print('Hello')
-except:
-    print("We have problems")
-else:
-    print("No problems")
+    numerator = int(input("Введіть чисельник: "))
+    denominator = int(input("Введіть знаменик:"))
+    result = numerator / denominator
+    print("Результат", result)
+except ZeroDivisionError:
+    print("Помилка: Ділення на нуль неможливе.")
+except ValueError:
+    print("Помилка: Введенні дані не є числом.")
