99f73c4981195ea7e050147897712873463a5d4b
Batovich228 <152063822+Batovich228@users.noreply.github.com>
2024-04-13 10:55:02 +0300

    Update les8.py

--- les8.py
+++ les8.py
@@ -1,10 +1,7 @@
 import logging
-# logging.basicConfig(level=logging.DEBUG, filename="logs.log", filemode="w")
-# logging.debug('debug message')
-# logging.info('info message')
-
-#з використанням параметру форматування
-
-logging.basicConfig(level=logging.debug, filename="logs.log", filemode="w", format="We have logging message:%(asctime)s:%(levelname)s -- %(message)s")
-logging.debug("This is a debug message")
-logging.info('This is an info message')
+logging.basicConfig(level=logging.DEBUG,
+filename="logs.log",
+filemode="w",
+format="We have next logging message:%(asctime)s:%(levelname)s - %(message)s")
+logging.debug('debug')
+logging.info('info')
